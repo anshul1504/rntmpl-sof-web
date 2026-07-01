b1f51c207bee1e55290c4647d3b93d4fcd2139ba
Anshul <anshulagrawal1504@gmail.com>
2026-07-01 13:34:33 +0530

    Fix admin import indentation and add jazzmin dependency

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+    from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.utils.html import format_html
 from django.urls import reverse
